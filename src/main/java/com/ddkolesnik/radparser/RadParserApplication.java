3c4a206fd8f098aa604aa99a27508ad2b3cfbe42
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-01-29 09:02:39 +0500

    Инициализация проекта

---
+++ src/main/java/com/ddkolesnik/radparser/RadParserApplication.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.radparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddkolesnik/radparser/RadParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.radparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
