b59463c075f1e105ebbcec53be2b9cc8e2eb81ab
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2021-01-31 21:41:00 +0500

    Добавлены классы для хранения информации о лоте

---
+++ src/main/java/com/ddkolesnik/radparser/model/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.ddkolesnik.radparser.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "creation_time")
+    private Instant creationTime;
+
+    @Column(name = "modified_time")
+    private Instant modifiedTime;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @PrePersist
+    public void prePersist() {
+        this.creationTime = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedTime = Instant.now();
+    }
+
+}
